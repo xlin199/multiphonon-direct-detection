9a855f75c4e68e588cdc7ab041db91e28dd87ec4
xlin199 <xil244@ucsd.edu>
2025-05-11 00:36:26 -0700

    renamed some variables and functions

--- StructureFactor1D.py
+++ StructureFactor1D.py
@@ -16,48 +16,68 @@ class StructureFactor1D:
         filter = np.argwhere(self.omegaList >= energyThreshold)
         self.omegaList = self.omegaList[filter].reshape(-1)
         self.nuList = np.arange(1, self.nLattice)[filter]
+        self.currIntS_diag = None # integrated diagonal structure factor (the integration bins are defined in setup_DoS)
+        self.sOrder = None # number of phonons corresponding to currIntS (or the order of currIntS)
 
-    def getDebyeWallerConst(self):
-        '''
-        Get W(q) appearing in the debye waller factor
-        '''
-        return self.q_value**2 / (4*self.mass*self.nLattice) * np.sum(1/self.omegaList)
-        
+    ######## Public functions ########
 
-    def get_diag_ini(self):
-        '''
-        Get IntS of order 1
-        '''
-        print('Processing n = 1...')
-        self.sOrder = 1
-        DWfactor =  np.exp(- 2 * self.getDebyeWallerConst()) 
-        s_values = 2 * np.pi / self.volume * self.nLattice * DWfactor * self.couplingConst**2 *  (self.q_value**2 /(2*self.mass*self.nLattice)) / self.omegaList
-        self.currIntS = self.get_int_s_diag(np.row_stack((self.omegaList, s_values)))
-        # return self.currIntS
-
-    def get_diag_rec(self,):
-        '''
-        Use the recursive relation to calculate the structure factor of nPhonon-th order
-        prevS: (2,a) array, first row: omega list 
-                            second row: structure factor integrated over omega in bins defined by setup_DoS (int_{omega}^{omega+Delta omega} S d(omega'))
-        --------returns--------
-        newS: (2,a*len(omegaList)) array
-        '''
-        newIntS = (self.q_value**2 /(2*self.mass*self.nLattice))/self.sOrder * self.currIntS[1,:, np.newaxis] / self.omegaList 
-        newOmega = self.currIntS[0,:, np.newaxis] + self.omegaList 
-        return np.row_stack((newOmega.T.reshape(-1,), newIntS.T.reshape(-1,)))
-    
     def setup_DoS(self,max_omega, nBins=500):
         '''
-        Helper function. Defines the bin used to calculate int_S
+        Defines the bin used to calculate IntS
         '''
         self._DoS_max_omega = max_omega
         self._DoS_min_omega = 0 
         self._DoS_nBins = nBins
         self._DoS_x_list, self._DoS_delta_omega = np.linspace(0, max_omega,nBins, retstep=True) 
 
+    def init_recursion(self):
+        print('Processing n = 1...')
+        self.sOrder = 1
+        self._s_diag_init()
+        # self.get_c_offdiag_init()
 
-    def get_int_s_diag(self,sFactor):
+    def update_s_factor(self):
+        '''
+        Update the integrated diagonal s factor (both diagonal and off diagonal) using the recursive relation.
+        '''
+        self.sOrder += 1
+        print(f'Processing n = {self.sOrder}...')
+        sFactor = self._calc_diag_s_rec()
+        self.currIntS_diag = self._integrate_s_diag(sFactor)
+
+    def get_binned_s_diag(self, xlist, delta_omega):
+        ''' 
+        Gives the binned diagonal (incoherent) part of s factor: int_{each bin} d(omega') S(q, omega') / (bin size)
+        xlist: x values used for plotting
+        delta_omega: bin size, or step size of the xlist
+        --------returns--------
+        ylist: array with same shape as xlist 
+        '''
+        allowed_omegas = self.currIntS_diag[0,:]
+        s_diag = self.currIntS_diag[1,:]
+        result = binned_statistic(allowed_omegas, s_diag, statistic="sum",bins=len(xlist), range=(min(xlist), max(xlist)))
+        ylist = result.statistic/delta_omega
+        return ylist
+    
+    ######## Helper functions ########
+        
+    def _DebyeWallerConst(self):
+        '''
+        Get W(q) appearing in the debye waller factor
+        '''
+        return self.q_value**2 / (4*self.mass*self.nLattice) * np.sum(1/self.omegaList)
+
+
+    def _s_diag_init(self):
+        '''
+        Get integrated diagonal s factor of order 1
+        '''
+        DWfactor =  np.exp(- 2 * self._DebyeWallerConst()) 
+        s_values = 2 * np.pi / self.volume * self.nLattice * DWfactor * self.couplingConst**2 *  (self.q_value**2 /(2*self.mass*self.nLattice)) / self.omegaList
+        self.currIntS_diag = self._integrate_s_diag(np.row_stack((self.omegaList, s_values)))
+        # return self.currIntS_diag
+
+    def _integrate_s_diag(self,sFactor):
         '''
         Helper function. Integrate the structure factor over omega in bins defined by setup_DoS (int_{omega}^{omega+Delta omega} S d(omega')) 
         --------returns--------
@@ -69,30 +89,18 @@ class StructureFactor1D:
         # np.nan_to_num(result.statistic, nan=0.0)
         return np.row_stack((self._DoS_x_list,result.statistic))
     
-
-    def update_s_diag(self):
+    def _calc_diag_s_rec(self,):
         '''
-        Update the integrated diagonal s factor using the recursive relation.
+        Helper function. 
+        Use the recursive relation to calculate the structure factor of nPhonon-th order
+        --------returns--------
+        newS: (2,a*len(omegaList)) array
         '''
-        self.sOrder += 1
-        print(f'Processing n = {self.sOrder}...')
-        sFactor = self.get_diag_rec()
-        self.currIntS = self.get_int_s_diag(sFactor)
+        newIntS = (self.q_value**2 /(2*self.mass*self.nLattice))/self.sOrder * self.currIntS_diag[1,:, np.newaxis] / self.omegaList 
+        newOmega = self.currIntS_diag[0,:, np.newaxis] + self.omegaList 
+        return np.row_stack((newOmega.T.reshape(-1,), newIntS.T.reshape(-1,)))
     
 
-    def get_binned_s_diag(self, xlist, delta_omega):
-        ''' 
-        Call this function to get the binned s factor: int_{each bin} d(omega') S(q, omega') / (bin size)
-        xlist: x values used for plotting
-        delta_omega: bin size, or step size of the xlist
-        --------returns--------
-        ylist: array with same shape as xlist 
-        '''
-        allowed_omegas = self.currIntS[0,:]
-        s_diag = self.currIntS[1,:]
-        result = binned_statistic(allowed_omegas, s_diag, statistic="sum",bins=len(xlist), range=(min(xlist), max(xlist)))
-        ylist = result.statistic/delta_omega
-        return ylist
 
 
 ########## The functions below are used for direct calculations (no recursive relation) ##############
@@ -168,55 +176,3 @@ class StructureFactor1D_NIndepDW(StructureFactor1D):
 
 
 ################# Sample code #######################
-
-# omega_dw = 8.194e-6
-# multplier = 2.5
-# q_value  = multplier*np.sqrt(2*26161e3*13.124e-6) # uses averaged omega (not inverse of averaged inverse)
-
-
-# nLattice = 1000
-# test = StructureFactor1D_NIndepDW(nLattice = nLattice,q_value=q_value, couplingConst=28,)
-# fmt = ['r-', 'g-', 'b-', 'y-', 'c-']
-# max_omega = 150*1e-6 # keV
-# xlist, delta_omega = np.linspace(0, max_omega,50, retstep=True) 
-# nPhonon = 40
-
-# # impulse approx
-# deltasq = q_value**2 * 13.124e-6/(2*test.mass)
-# ia =  28**2 / (8/(test.latticeConst**3)) * np.sqrt(2*np.pi/deltasq) * np.exp(- (xlist-(q_value**2/(2*test.mass)))**2 / (2*deltasq)) # uses averaged omega (not inverse of averaged inverse)
-
-# sum = np.zeros(xlist.shape)
-# fig, ax = plt.subplots(figsize=(8,6))
-# test.setup_DoS(max_omega, nBins=2000)
-# test.get_diag_ini()
-# for i in range(1,nPhonon):
-#     s_diag = test.get_binned_s_diag(xlist, delta_omega) # get the binned s factor of order i
-#     s_diag = np.nan_to_num(s_diag, nan=0.0) # cleaning up the nan
-#     sum += s_diag # update the sum of s factor
-#     print(f'Plotting n = {i}...')
-#     # plot the current s factor
-#     ax.plot(xlist*1e6, s_diag,label=f'n = {i}') 
-#     formatter = ScalarFormatter(useMathText=True)
-#     formatter.set_powerlimits((8,8))
-#     ax.yaxis.set_major_formatter(formatter)
-#     # calculate the s factor of next order
-#     test.update_s_diag()
-# # plotting the last s factor
-# print(f'Plotting n = {nPhonon}...')
-# ax.plot(xlist*1e6, s_diag,label=f'n = {nPhonon}')
-# formatter = ScalarFormatter(useMathText=True)
-# formatter.set_powerlimits((8,8))
-# ax.yaxis.set_major_formatter(formatter)
-# # plot the impulse approximation
-# ax.plot(xlist*1e6, ia, linestyle="--", color="gray", label='Impulse')
-# # plot the sum of s for all orders
-# ax.plot(xlist*1e6,sum, label="Total",color="black")
-# # labels & titles
-# txt1 = r"$\omega_{DW}$" #r"exp$(\frac{q^2}{4m\omega_{DW}})$"
-# txt2 = r"$\sqrt{2m\bar\omega}$" 
-# plt.ylim(-0.1e8, 7.2e8)
-# plt.title(f'DoS + Incoherent approx, N-independent DW, Recursive\nN = {nLattice}, {txt1} = {omega_dw*1e6} meV, q = {multplier}{txt2}')# , {txt} = {test.omegaNaught*1e6} meV
-# plt.ylabel('Structure factor')
-# plt.xlabel(r'$\omega$[meV]')
-# plt.legend()
-# plt.savefig(f"sfactorwrtomega_q{int(q_value)}_{nLattice}lattice_NindepDW_energyCut_recur_DoS", bbox_inches="tight")
