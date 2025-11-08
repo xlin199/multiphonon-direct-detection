1c485126213d2e1e97a455570f391dc08a59978e
xlin199 <xil244@ucsd.edu>
2025-11-07 19:27:52 -0800

    n site lattice rate

--- NSiteLatticeCoherentMultiprocess.py
+++ NSiteLatticeCoherentMultiprocess.py
@@ -12,7 +12,7 @@ def multi_processing_helper(q_value, nPhonon,
     sFacCalc = StructureFactor1D(nLattice = nLattice,q_value=q_value, couplingConst=28,)
     xlist, delta_omega = np.linspace(0, max_omega,omegalength, retstep=True) 
     s_tot = np.zeros(xlist.shape)
-    sFacCalc.setup_DoS(max_omega, nBins=1200)
+    sFacCalc.setup_DoS(max_omega, nBins=1500)
     sFacCalc.init_recursion(print_message=False)
     s_diag = sFacCalc.get_binned_s_diag(xlist, delta_omega)
     s_offdiag = sFacCalc.get_binned_s_offdiag(xlist, delta_omega)
@@ -29,7 +29,7 @@ def multi_processing_helper(q_value, nPhonon,
         s_tot += s_offdiag
     return s_tot
 
-omegalength=1200
+omegalength=1500
 max_omega = 250*1e-6
 nLattice=120
 phonon_func = partial(multi_processing_helper, omegalength=omegalength, nLattice=nLattice, max_omega = max_omega)
@@ -39,10 +39,10 @@ nPhonon20func = partial(phonon_func, nPhonon = 20)
 if __name__ == "__main__":
     omegalist = np.linspace(0, max_omega,omegalength, ) 
     qscale = np.sqrt(2*26161e3*13.13e-6) # uses averaged omega (not inverse of averaged inverse)
-    qlenth = 1000
+    qlenth = 1200
     qlist = np.linspace(0.0004, 2.5*qscale, qlenth) # 2.5 sqrt(2m omega_bar)
     print("Start time:", datetime.now())
-    with Pool(cpu_count()//3) as pool:
+    with Pool(cpu_count()//2) as pool:
         result10phonon = pool.map(nPhonon10func, qlist[:qlenth//2])
         result20phonon = pool.map(nPhonon20func, qlist[qlenth//2:])
     final_results = np.concatenate((result10phonon, result20phonon)) # shape: (q length, omega length)
