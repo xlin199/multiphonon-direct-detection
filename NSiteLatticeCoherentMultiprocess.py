962898dcf53843bf468c8d4402871085feaa0c8f
xlin199 <xil244@ucsd.edu>
2025-11-10 13:47:02 -0800

    add incoherent approx output

--- NSiteLatticeCoherentMultiprocess.py
+++ NSiteLatticeCoherentMultiprocess.py
@@ -12,11 +12,13 @@ def multi_processing_helper(q_value, nPhonon,
     sFacCalc = StructureFactor1D(nLattice = nLattice,q_value=q_value, couplingConst=28,)
     xlist, delta_omega = np.linspace(0, max_omega,omegalength, retstep=True) 
     s_tot = np.zeros(xlist.shape)
+    s_diag_tot = np.zeros(xlist.shape)
     sFacCalc.setup_DoS(max_omega, nBins=1500)
     sFacCalc.init_recursion(print_message=False)
     s_diag = sFacCalc.get_binned_s_diag(xlist, delta_omega)
     s_offdiag = sFacCalc.get_binned_s_offdiag(xlist, delta_omega)
     # update the sum of s factor
+    s_diag_tot += s_diag 
     s_tot += s_diag 
     s_tot += s_offdiag
     for i in range(2, nPhonon+1):
@@ -25,11 +27,12 @@ def multi_processing_helper(q_value, nPhonon,
         s_diag = sFacCalc.get_binned_s_diag(xlist, delta_omega)
         s_offdiag = sFacCalc.get_binned_s_offdiag(xlist, delta_omega)
         # update the sum of s factor
+        s_diag_tot += s_diag 
         s_tot += s_diag 
         s_tot += s_offdiag
-    return s_tot
+    return s_diag_tot, s_tot
 
-omegalength=1500
+omegalength=1200
 max_omega = 250*1e-6
 nLattice=120
 phonon_func = partial(multi_processing_helper, omegalength=omegalength, nLattice=nLattice, max_omega = max_omega)
@@ -43,11 +46,15 @@ if __name__ == "__main__":
     qlist = np.linspace(0.0004, 2.5*qscale, qlenth) # 2.5 sqrt(2m omega_bar)
     print("Start time:", datetime.now())
     with Pool(cpu_count()//2) as pool:
-        result10phonon = pool.map(nPhonon10func, qlist[:qlenth//2])
-        result20phonon = pool.map(nPhonon20func, qlist[qlenth//2:])
-    final_results = np.concatenate((result10phonon, result20phonon)) # shape: (q length, omega length)
+        allresult10phonon = pool.map(nPhonon10func, qlist[:qlenth//2])
+        allresult20phonon = pool.map(nPhonon20func, qlist[qlenth//2:])
+    res10diag, res10 = zip(*allresult10phonon)
+    res20diag, res20 = zip(*allresult20phonon)
+    finalresdiag = np.concatenate((res10diag, res20diag))
+    finalres = np.concatenate((res10, res20))
+     # shape: (q length, omega length)
     print("End time:", datetime.now())
-    print(final_results.shape)
-    np.savez('testMultiprocess', final_results=final_results, qlist=qlist, omegalist=omegalist)
+    print(finalres.shape)
+    np.savez('testMultiprocess',s_tot=finalres, s_diag=finalresdiag, qlist=qlist, omegalist=omegalist)
 
 
